1108e3206003f9376e1e96be4b00e5d03d153e86
humphrey <humpasp@gmail.com>
2021-04-09 15:38:40 +0300

    StayClean

---
+++ src/components/Body.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="container">
+      <div v-if="state"><img src="../assets/source.gif" /></div>
+      <div id="off" v-else></div>
+    </div>
+    <button class="btn btn-primary-danger" v-on:click="changeState">
+      <span v-if="state"> Off</span>
+      <span v-else>On</span>
+    </button>
+  </div>
+
+
+  <div>
+<h3>Student Registration</h3>
+<ol></ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Body',
+  data: function () {
+    return {
+      state: false,
+    };
+  },
+
+  methods: {
+    changeState: function () {
+      this.state = !this.state;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 420px;
+  align-content: center;
+  background-color: rgb(10, 10, 10);
+  width: 500px;
+  height: 500px;
+  border: 15px solid rgb(11, 12, 11);
+  margin: center;
+  background-size: cover;
+  border-radius: 10px;
+}
+
+button {
+  position: relative;
+  margin-top: -70px;
+  border-radius: 100%;
+  transition: 0.5s;
+  border: 1px solid red;
+}
+img {
+  background: center;
+  position: relative;
+  width: 420px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+   
+    <Body/>
+    
+  </div>
+</template>
+
+<script>
+
+import Body from "./Body.vue"
+
+export default {
+  name: 'Home',
+  components:{
+    
+    Body,
+    
+   
+
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
